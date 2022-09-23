a59ea68c1e6698a8d367a276257c83868b320c66
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-09-23 17:06:29 +0900

    feat: Add voting logic

--- run_recognition.py
+++ run_recognition.py
@@ -34,7 +34,7 @@ class RecognitionDemo(object):
         self.action_labels = {0 : "big_wind", 1 : "bokbulbok", 2 : "chalseok_chalseok_phaldo", 3 : "chulong_chulong_phaldo", 4 : "crafty_tricks"}
 
     def preds2label(self, confidence):
-        k = 4
+        k = 5
         class_scores, class_inds = torch.topk(confidence, k=k)
         labels = {
             self.action_labels[int(class_inds[j])]: float(class_scores[j].item())
@@ -59,45 +59,58 @@ class RecognitionDemo(object):
         poses_list = []
         predicted_labels = []
         for img in self.image_provider:
+            # img = cv2.resize(img, (1080, 720))
+            height, width, _ = img.shape
+            # print(height, width)
             start_time = time.perf_counter()
             poses = self.pose_estimator.infer(img)
             
-            for pose in poses:
-                draw(img, pose)
-                
-            if len(poses) > 0:
-                counter += 1
-                poses_list.append(poses)
+            if not len(poses) == 0:
+                for pose in poses:
+                    draw(img, pose)
+                    
+                if len(poses) > 0:
+                    counter += 1
+                    poses_list.append(poses)
 
-            if counter > self.data_extractor.total_frames:
-                poses_list.pop(0)
-                counter = self.total_frames
+                if counter > self.data_extractor.total_frames:
+                    poses_list.pop(0)
+                    counter = self.data_extractor.total_frames
 
-            if counter > 0:
-                skeleton_seq = self.data_extractor.pose2numpy(counter, poses_list)
+                if counter > 0:
+                    skeleton_seq = self.data_extractor.pose2numpy(counter, poses_list)
 
-                prediction = self.action_classifier.infer(skeleton_seq)
-                category_labels = self.preds2label(prediction.confidence)
-                self.draw_preds(img, category_labels)
+                    prediction = self.action_classifier.infer(skeleton_seq)
+                    # print(prediction)
+                    skeleton_seq = []
+                    category_labels = self.preds2label(prediction.confidence)
+                    # print(category_labels)
+                    if max(list(category_labels.values())) > 0.7:
+                        # self.draw_predgit ss(img, category_labels)
 
-            predicted_label = torch.argmax(prediction.confidence)
-            predicted_labels.append(predicted_label.item())
-            if len(predicted_labels) == 50:
+                        predicted_label = torch.argmax(prediction.confidence)
+                        predicted_labels.append(predicted_label.item())
+                        
+            voting_label = ""
+            if len(predicted_labels) == 60:
                 voting_result = max(predicted_labels, key=predicted_labels.count)
                 predicted_labels.clear()
-                print(voting_result)
-            
+                
+                voting_label = self.action_labels[voting_result]
+            print(voting_label)
+            # if len(voting_label) > 0:
+            #     voting_label
             end_time = time.perf_counter()
             fps = 1.0 / (end_time - start_time)
             avg_fps = 0.8 * fps + 0.2 * fps
-            img = cv2.putText(img,"FPS: %.2f" % (avg_fps,),(100, 160),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2,cv2.LINE_AA,)
+            img = cv2.putText(img,"FPS: %.2f" % (avg_fps,),(100, 400),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2,cv2.LINE_AA,)
+            img = cv2.putText(img, voting_label,(100, 220),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 0, 255),2)
             cv2.imshow("Result", img)
             key = cv2.waitKey(1)
             if key == ord("q"):
-                break
+                break      
 
 if __name__ == "__main__":
-    path = "/media/lakpa/Storage/youngdusan_data/all_resized_videos/chulong_chulong_phaldo_361.mp4"
-
+    path = "./resources/test_videos/wind_that_shakes_trees_602.mp4"
     recdem = RecognitionDemo(video_path=path)
     recdem.prediction()
