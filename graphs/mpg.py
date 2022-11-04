4a0f03f132d7a780cc22829c7d55c8575000b35b
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-11-04 12:35:35 +0900

    doc: Add docstring to the methods

--- graphs/mpg.py
+++ graphs/mpg.py
@@ -18,6 +18,17 @@ neighbor = in_edge + out_edge
 
 
 def get_hop(link, num_node):
+    """Calculates the identity matrix 
+
+    Args:
+        link (array of tuples): Edge connections between nodes
+        num_node (int): Total number of nodes in the skeleton
+
+    Returns:
+        ndarray: Identity matrix
+        For example: The indentity matrix of 3 nodes is 
+        [[1,0,0],[0,1,0],[0,0,1]]
+    """    
     A = np.zeros((num_node, num_node))
     for i, j in link:
         A[j, i] = 1
@@ -25,6 +36,14 @@ def get_hop(link, num_node):
 
 
 def normalize_digraph(A):
+    """Normalizes the graph value and returns the adjancy matrix
+
+    Args:
+        A (ndarray): Identity matrix
+
+    Returns:
+        ndarray: Adjacency matrix
+    """    
     Dl = np.sum(A, 0)
     h, w = A.shape
     Dn = np.zeros((w, w))
@@ -36,9 +55,20 @@ def normalize_digraph(A):
 
 
 def get_spatial_graph(num_node, self_link, in_edge, out_edge):
-    I = get_hop(self_link, num_node)
-    In = normalize_digraph(get_hop(in_edge, num_node))
-    Out = normalize_digraph(get_hop(out_edge, num_node))
+    """_summary_
+
+    Args:
+        num_node (int): _description_
+        self_link (list of tuple): links between self node. Node A has link to Node A.
+        in_edge (list of tuple): links between inside edges
+        out_edge (list of tuple): links between outside edge
+
+    Returns:
+        ndarray: Adjancy matrix containing information about the graph structure
+    """    
+    I = get_hop(self_link, num_node)    # For self links i.e., links between self node. Node A has link to Node A.
+    In = normalize_digraph(get_hop(in_edge, num_node))  # For in_edge, i.e., links between inside edges
+    Out = normalize_digraph(get_hop(out_edge, num_node))    # For out_edge, i.e., links between outside edge
     A = np.stack((I, In, Out))
     return A
 
